0ee4a77ccbacbc36d55806e3a74a70f4c13d1ee0
Lollygagger <ubercharge60@gmail.com>
2024-11-08 10:33:48 -0500

    Fixing base dir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'lollygagger.github.io/',
+  base: '/',
   build: {
     outDir: 'dist', // Output folder for the build
     emptyOutDir: true, // Clear out dist folder before building
